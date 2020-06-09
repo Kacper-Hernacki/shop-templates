2b6a9d3b1d9d6ca22eb3a76deeb46a735efe1754
Kacper <hernackikacper@gmail.com>
2020-06-09 21:21:31 +0200

    after

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,29 @@
+
+// Translation  
 var arrLang = {
     'en' : {
-        'home' : 'Home'
+        'home' : 'Home',
+        'about' : 'About',
+        'products' : 'Products',
+        'gallery' : 'Gallery',
+        'contact' : 'Contact Us',
+        'go-shop' : 'Go to shop',
+        'main-1-1' : 'Welcome to my website!',
+        'main-1-2' : 'You will find here handmade products',
+        'main-2' : 'Every handicraft is made with passion',
+        'main-3' : 'See unique products which are not available in ordinary shop'
     },
     'pl' : {
-        'home' : 'Strona Główna'
+        'home' : 'Strona Główna',
+        'about' : 'O mnie',
+        'products' : 'Produkty',
+        'gallery' : 'Galeria',
+        'contact' : 'Kontakt',
+        'go-shop' : 'Przejdź do sklepu',
+        'main-1-1' : 'Witaj na mojej stronie!',
+        'main-1-2' : 'Znajdziesz tu ręcznie wykonane produkty',
+        'main-2' : 'Każde rękodzieło jest robione z pasją',
+        'main-3' : 'Zobacz unikatowe produkty, których nie nabędziesz w zwykłym sklepie'
     }
 };
 
