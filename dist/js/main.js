616bd7e764fc627ecdfa3d44eeb19fb3361626d3
Kacper <hernackikacper@gmail.com>
2020-06-15 20:33:46 +0200

    about

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,7 +11,8 @@ var arrLang = {
         'main-1-1' : 'Welcome to my website!',
         'main-1-2' : 'You will find here handmade products',
         'main-2' : 'Every handicraft is made with passion',
-        'main-3' : 'See unique products which are not available in ordinary shop'
+        'main-3' : 'See unique products which are not available in ordinary shop',
+        'quote' : 'Style is a way to say who you are without having to speak.'
     },
     'pl' : {
         'home' : 'Strona Główna',
@@ -23,7 +24,8 @@ var arrLang = {
         'main-1-1' : 'Witaj na mojej stronie!',
         'main-1-2' : 'Znajdziesz tu ręcznie wykonane produkty',
         'main-2' : 'Każde rękodzieło jest robione z pasją',
-        'main-3' : 'Zobacz unikatowe produkty, których nie nabędziesz w zwykłym sklepie'
+        'main-3' : 'Zobacz unikatowe produkty, których nie nabędziesz w zwykłym sklepie',
+        'quote' : 'Styl to sposób w jaki bez słów ukazujesz siebie.'
     }
 };
 
